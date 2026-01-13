b2b36112ba91b79f6b08661dd8ad6df7717a0ecf
Ajay Gour <theajaygour@gmail.com>
2026-01-13 09:53:32 +0530

    Update App.jsx

--- class16_contextapiMore/src/App.jsx
+++ class16_contextapiMore/src/App.jsx
@@ -6,6 +6,7 @@ import Footer from './components/Footer'
 const App = () => {
   return (
     <div>
+      {/* link navlink and a tag are same */}
       {/* <Navbar > 
        <h1>this is children od nav</h1> 
        <h1>this is another children od nav</h1> 
