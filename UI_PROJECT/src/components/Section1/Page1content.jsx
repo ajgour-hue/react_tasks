d2bb10c49be91071dfb1ceb7b3fba76390503535
Ajay Gour <theajaygour@gmail.com>
2026-02-22 22:19:57 +0530

    page1

--- UI_PROJECT/src/components/Section1/Page1content.jsx
+++ UI_PROJECT/src/components/Section1/Page1content.jsx
@@ -2,11 +2,11 @@ import React from 'react'
 import LeftContent from './LeftContent'
 import RightContent from './RightContent'
 
-const Page1content = () => {
+const Page1content = (props) => {
   return (
     <div className=' flex w-full py-8 px-18 gap-5 h-[90vh]'>
       <LeftContent />
-      <RightContent />
+      <RightContent users = {props.users} />
     </div>
   )
 }
