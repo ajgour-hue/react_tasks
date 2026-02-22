8d009a42309fec8ed8be8b1671204943c327c04d
Ajay Gour <theajaygour@gmail.com>
2026-02-22 22:19:25 +0530

    btn-left

---
+++ UI_PROJECT/src/components/Section1/Btn.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Btn = () => {
+  return (
+    <div>
+      <div>
+           <button className='mb-20 cursor-pointer rounded-full bg-black text-white px-6 py-2 tracking-widest text-sm'>Check here</button>
+      </div>
+    </div>
+  )
+}
+
+export default Btn
