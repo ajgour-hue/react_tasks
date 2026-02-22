06dcb2d955c223a12f1b27fa965f40202ef5055d
Ajay Gour <theajaygour@gmail.com>
2026-02-22 22:19:44 +0530

    left

--- UI_PROJECT/src/components/Section1/LeftContent.jsx
+++ UI_PROJECT/src/components/Section1/LeftContent.jsx
@@ -1,15 +1,12 @@
 import React from 'react'
+import HomeText from './HomeText'
+import Btn from './Btn'
 
 const LeftContent = () => {
   return (
     <div className=' w-1/3 h-full flex flex-col justify-between'>
-      <div>
-        <h3 className='mt-20 mb-8 text-6xl font-bold'>Prospective <br /> <span>Customer</span> <br /> Segmentation </h3>
-      <p className=' text-xl '>Lorem ipsum dolor sit amet consectetur adipisicing elit. Perferendis ipsam nostrum cupiditate, accusantium iste veritatis nisi aliquid neque deserunt nihil minus. Debitis distinctio eaque consequuntur perferendis quasi, et dolorem odit.</p>
-      </div>
-      <div>
-        <button className='mb-20 cursor-pointer rounded-full bg-black text-white px-6 py-2 tracking-widest text-sm'>Check here</button>
-      </div>
+     <HomeText />
+     <Btn />
     </div>
 
   )
