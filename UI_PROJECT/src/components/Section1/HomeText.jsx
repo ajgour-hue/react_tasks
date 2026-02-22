47a302b0ed0e1b0cbe0c8ce2456fd6fbef073d40
Ajay Gour <theajaygour@gmail.com>
2026-02-22 22:19:35 +0530

    left-content

---
+++ UI_PROJECT/src/components/Section1/HomeText.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const HomeText = () => {
+  return (
+    <div>
+       <div>
+        <h3 className='mt-20 mb-8 text-6xl font-bold'>Prospective <br /> <span>Customer</span> <br /> Segmentation </h3>
+      <p className=' text-xl '>Lorem ipsum dolor sit amet consectetur adipisicing elit. Perferendis ipsam nostrum cupiditate, accusantium iste veritatis nisi aliquid neque deserunt nihil minus. Debitis distinctio eaque consequuntur perferendis quasi, et dolorem odit.</p>
+      </div>
+    </div>
+  )
+}
+
+export default HomeText
